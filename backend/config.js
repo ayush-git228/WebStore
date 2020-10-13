04f54f728ca9427ace8d709e22fefd4859ccd8e1
ayush-git228 <66081505+ayush-git228@users.noreply.github.com>
2020-10-13 09:16:23 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 module.exports={
   "PORT": "process.env.PORT || 5000",
-  "MONGODB_URL": "process.env.MONGODB_URL || mongodb://localhost/tracker",
+  "MONGODB_URL": "process.env.MONGODB_URL || mongodb://localhost/webstore",
   "JWT_SECRET": "process.env.JWT_SECRET || somethingsecret",
   "PAYPAL_CLIENT_ID": "process.env.PAYPAL_CLIENT_ID || sb",
   "accessKeyId": "process.env.accessKeyId || accessKeyId",
