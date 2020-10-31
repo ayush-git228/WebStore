c9310cda4ce159969f03ed8c671687d927fd0566
Ayush Gupta <66081505+ayush-git228@users.noreply.github.com>
2020-11-01 00:10:23 +0530

    Cors setting

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,39 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const config = require('./config');
 const userRoute = require('./routes/userRoute');
 const productRoute = require('./routes/productRoute');
 const orderRoute = require('./routes/orderRoute');
-const uploadRoute = require('./routes/uploadRoute');
-const request = require("request");
 
-const mongodbUrl =  "process.env.MONGODB_URL || mongodb://localhost/tracker";
+const mongodbUrl =  "mongodb://localhost:27017/tracker";
 mongoose
   .connect(mongodbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+  }).then(()=>{
+    console.log("Connected Successfully to the database");
   })
   .catch((error) => error.reason);
-
+ 
 const app = express();
 const port=process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use('/api/uploads', uploadRoute);
+app.use(require('cors')());       // CORS MIDDLEWARE
+
+app.use(express.json());
+
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/orders', orderRoute);
+
 app.get('/api/config/paypal', (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
-app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
+
+//app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/../frontend/build')));  // Added '/' extra
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/public/index.html`));
 });
