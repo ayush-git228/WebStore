1bcc9ce46f113af19816c74e0922c83be66db178
Ayush Gupta <66081505+ayush-git228@users.noreply.github.com>
2020-11-01 00:02:24 +0530

    made changes in order reducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -3,7 +3,8 @@ import {
   ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL,
   ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL,
   MY_ORDER_LIST_REQUEST, MY_ORDER_LIST_SUCCESS, MY_ORDER_LIST_FAIL,
-  ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_LIST_FAIL, ORDER_DELETE_REQUEST, ORDER_DELETE_SUCCESS, ORDER_DELETE_FAIL
+  ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_LIST_FAIL, ORDER_DELETE_REQUEST, ORDER_DELETE_SUCCESS, ORDER_DELETE_FAIL,
+  ORDER_DELIVER_REQUEST, ORDER_DELIVER_SUCCESS, ORDER_DELIVER_FAIL, ORDER_DELIVER_RESET
 } from "../constants/orderConstants";
 
 
@@ -102,7 +103,21 @@ function orderDeleteReducer(state = {
     default: return state;
   }
 }
-export {
-  orderCreateReducer, orderDetailsReducer,
-  orderPayReducer, myOrderListReducer, orderListReducer, orderDeleteReducer
-}
+
+const orderDeliverReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_DELIVER_REQUEST:
+      return { loading: true };
+    case ORDER_DELIVER_SUCCESS:
+      return { loading: false, success: true };
+    case ORDER_DELIVER_FAIL:
+      return { loading: false, error: action.payload };
+    case ORDER_DELIVER_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export { orderCreateReducer, orderDetailsReducer, orderPayReducer, myOrderListReducer, 
+  orderListReducer, orderDeleteReducer, orderDeliverReducer }
