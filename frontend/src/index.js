4708b8ea76dd9d51ba5a9077716493b0ed4407b8
Ayush Gupta <66081505+ayush-git228@users.noreply.github.com>
2020-11-01 00:06:22 +0530

    font-awesome lib add

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'font-awesome/css/font-awesome.min.css';
 import App from './App';
 import store from './store';
 
