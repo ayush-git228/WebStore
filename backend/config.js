e0e217c7db3472e2a3a5523ac5bfab03c122f973
Ayush Gupta <66081505+ayush-git228@users.noreply.github.com>
2020-11-01 00:13:09 +0530

    Key changes

--- backend/config.js
+++ backend/config.js
@@ -6,7 +6,7 @@ module.exports={
   "PORT": "process.env.PORT || 5000",
   "MONGODB_URL": "process.env.MONGODB_URL || mongodb://localhost/webstore",
   "JWT_SECRET": "process.env.JWT_SECRET || somethingsecret",
-  "PAYPAL_CLIENT_ID": "process.env.PAYPAL_CLIENT_ID || sb",
+  "PAYPAL_CLIENT_ID": "client id we can get from developers for paypal",
   "accessKeyId": "process.env.accessKeyId || accessKeyId",
   "secretAccessKey": "process.env.secretAccessKey || secretAccessKey",
 };
