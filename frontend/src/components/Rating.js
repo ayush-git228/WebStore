c492d7d20778f81a9fc4672fd0287e95c547fe03
Ayush Gupta <66081505+ayush-git228@users.noreply.github.com>
2021-05-11 02:59:02 +0530

    Update Rating.js (#11)

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Rating(props) {
   return !props.value ? (
     <div></div>
-  ) : (
+    ) : (
     <div className="rating">
       <span>
         <i
